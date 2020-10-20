8c8a7c1f389eb85014ea334c19709f789eb54f51
jipijipi <jp.leflot@gmail.com>
2020-10-20 15:40:07 +0200

    its working now

--- library/script.js
+++ library/script.js
@@ -1,12 +1,19 @@
 let myLibrary = [];
 
+//book constructor
+
 function Book(title, author, pages, hasRead = false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
     this.info = () => `The book is ${this.title} by ${this.author}, it has ${this.pages} pages and ${!this.hasRead ? "I did not read it yet." : "I already read it."}`;
+    this.toggleRead = () => {
+        this.hasRead ? this.hasRead = false : this.hasRead = true;
+        updateLibrary()
+    };
 }
+
 
 function addBook(title, author, pages, hasRead) {
     myLibrary.push(new Book(title, author, pages, hasRead));
@@ -20,17 +27,24 @@ function updateLibrary() {
     let library = document.querySelector('#library');
     library.innerHTML = "";
 
-    for (item of myLibrary) {
-
-        console.log(item);
+    for (let [index, item] of myLibrary.entries()) {
 
         let card = document.createElement('div');
         card.setAttribute('id', item.title);
         card.setAttribute('class', 'book');
-        card.innerHTML = `<h2>${item.title}</h2><p>${item.author}</p><p>${item.pages}</p><p>${item.hasRead}</p>`;
+        card.setAttribute(['data-index'], index);
+        card.innerHTML = `<h2>${item.title}</h2><p>${item.author}</p><p>${item.pages}</p><p>${item.hasRead}</p><button class="btnRemove">-</button><button class="btnRead">read</button>`;
         library.append(card);
 
     }
+
+    //remove buttons
+    let removeButtons = document.querySelectorAll('.btnRemove');
+    removeButtons.forEach(x => x.addEventListener('click', removeBook));
+
+    //toogle read
+    let toggleRead = document.querySelectorAll('.btnRead');
+    toggleRead.forEach(x => x.addEventListener('click', toggleReadBook))
 }
 
 updateLibrary();
@@ -40,19 +54,28 @@ updateLibrary();
 function submitBook() {
     let bookForm = document.querySelector('#bookForm');
     let title = bookForm.querySelector(['[name = "title"]']);
-    let details = bookForm.querySelectorAll('.detail');
-    let checks = bookForm.querySelectorAll('.checkbox').checked;
+    let author = bookForm.querySelector(['[name = "author"]']);
+    let pages = bookForm.querySelector(['[name = "pages"]']);
+    let hasRead = bookForm.querySelector(['[name = "hasRead"]']);
 
-    addBook(title.value);
-    details.forEach(x => console.log("ee", myLibrary[myLibrary.length - 1][x.getAttribute('name')]));
-
-    console.log('input : ' + title.value);
-    details.forEach(x => console.log('input : ' + x.value));
-    console.log('input : ' + checks);
+    addBook(title.value, author.value, pages.value, hasRead.checked);
 
     updateLibrary();
 }
 
-//button click
+//add book click
 
 document.querySelector('#addBook').addEventListener('click', submitBook);
+
+//remove book click
+
+function removeBook() {
+    let cardIndex = this.parentNode.getAttribute('data-index');
+    myLibrary.splice(cardIndex, 1);
+    updateLibrary()
+}
+
+function toggleReadBook() {
+    let cardIndex = this.parentNode.getAttribute('data-index');
+    myLibrary[cardIndex].toggleRead();
+}
